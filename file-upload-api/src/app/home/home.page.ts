ec61c205180cd4fd1cde506b9b958d02be4d2eb2
RuneYap <runelaboratory94@gmail.com>
2020-06-29 07:11:41 +0800

    try input file

--- file-upload-api/src/app/home/home.page.ts
+++ file-upload-api/src/app/home/home.page.ts
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  text = 'default text';
   constructor() {}
 
+  onChangeText() {
+    this.text = 'Change JK';
+  }
+
+  loadImageFromDevice(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = () => {
+      const blob: Blob = new Blob([new Uint8Array((reader.result as ArrayBuffer))]);
+
+      const blobURL: string = URL.createObjectURL(blob);
+    };
+    console.log('Said something');
+  }
 }
